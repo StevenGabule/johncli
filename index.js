70563fa5464e7645b922e036defe3fea027d352e
John Paul <lucas.gabule@gmail.com>
2023-06-18 07:00:12 +0800

    add keywords and readme file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console')
+const pkgJSON = require('./package.json')
+const welcome = require('cli-welcome')
 
-clearConsole();
+welcome({
+	title: pkgJSON.name,
+	tagLine: 'Learn NodeJS - Creating CLI',
+	description: pkgJSON.description,
+	version: pkgJSON.version,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true
+})
 
 console.log(`
-
 John Paul Gabule - Software Developer
 
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nulla facere omnis autem modi optio laudantium veniam commodi excepturi doloremque? Nam doloribus velit dicta sit dignissimos libero, repellat ab hic enim quae aperiam, magni facilis eius quam harum impedit quidem quod corporis explicabo porro? Doloremque quis modi ratione aliquid temporibus tempora? Veritatis deleniti impedit, consectetur ipsa distinctio accusamus molestiae reprehenderit hic in ut sapiente sed temporibus. Quisquam, eius quae? Inventore aspernatur accusamus nisi! Libero est quis nulla explicabo minima fuga, aspernatur a labore eos placeat ea aliquid sint commodi veritatis aliquam natus similique maiores voluptates harum debitis fugit facilis unde?
---
+++ readme.md
@@ -0,0 +1,9 @@
+# `npx john-gabs`
+> Get to know NodeJS running on the node cli in your command line with node.js
+
+## Usage
+
+To use this CLI run the following command:
+```sh
+npx johngabs
+```
